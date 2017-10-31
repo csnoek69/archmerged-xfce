bf6a73392c9ebd9e60f010bdcef34c4fbddf5092
Corne Snoek <corne.snoek@gmail.com>
2017-10-31 10:41:50 +0100

    desktop added

--- install-utilities.sh
+++ install-utilities.sh
@@ -8,29 +8,11 @@
 #
 ##################################################################################################################
 
-# checking if I have the latest files from github
-echo "Checking for newer files online first"
-git pull
+set -e
 
-# Below command will backup everything inside the project folder
-git add --all .
-
-# Give a comment to the commit if you want
-echo "####################################"
-echo "Write your commit comment!"
-echo "####################################"
-
-read input
-
-# Committing to the local repository with a message containing the time details and commit text
-
-git commit -m "$input"
-
-# Push the local files to github
-
-git push -u origin master
+sudo pacman -S meld
 
 
 echo "################################################################"
-echo "###################    Git Push Done      ######################"
+echo "###################    Install Done       ######################"
 echo "################################################################"
