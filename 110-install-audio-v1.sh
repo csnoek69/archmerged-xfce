8f711939d14e6d99dff4f0f560185cd9778027fd
Corne Snoek <corne.snoek@gmail.com>
2017-10-31 11:18:07 +0100

    added audio

---
+++ 110-install-audio-v1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+##################################################################################################################
+# Author 	: 	Corne Snoek
+# Website 	: 	http://www.esoxict.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+set -e
+
+sudo pacman -S pulseaudio pulseaudio-alsa --noconfirm --needed
+sudo pacman -S alsa-utils alsa-plugins alsa-firmware alsa-lib --noconfirm --needed
+sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly  gstreamer  --noconfirm --needed
+
+
+echo "################################################################"
+echo "###################    Install Done       ######################"
+echo "################################################################"
